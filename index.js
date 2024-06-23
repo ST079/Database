ec2e16dab172e44c79a899b70186648e0f2e8389
Sujan Tamang <suzanyba079@gmail.com>
2024-06-23 19:44:25 +0545

    mongoose and database connection

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const indxRouter = require("./routes/index-route");
 const errorMiddleware = require("./middlewares/error-middleware");
 const app = express();
 const PORT = 8000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/blog-application").then(() => {
+  console.log("Database is connected");
+});
 
 app.use(express.json());
 app.use(express.static("public"));
